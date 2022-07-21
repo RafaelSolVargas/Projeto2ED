5f33a54db4a8202426910dd3700681f59ca069b4
Thiago Augusto Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-07-21 16:00:11 -0300

    changed order of code to make it work (not clean code)

--- main.cpp
+++ main.cpp
@@ -5,80 +5,88 @@
 
 using namespace std;
 
+void buildTrie(Trie *tree, string dictToOpen) {}
+
 int main() {
-    // cin >> dictFileName; // entrada
-    // string dictFileName;
-    // cout << filename << endl; // esta linha deve ser removida
-    fstream dictFile;
-    string texto;
-    // Lê o arquivo dic e coloca no objeto texto
-    dictFile.open("Dictionaries/dic1.dic", ios::in);
-    if (dictFile.is_open()) {
+  // cin >> dictFileName; // entrada
+  // string dictFileName;
+  // cout << filename << endl; // esta linha deve ser removida
+  //
+  // Processa a entrada e alimenta a árvore
+  // string keys[] = {"bear", "beara", "there", "answer", "any", "by", "bye",
+  // "their"}; int n = sizeof(keys) / sizeof(keys[0]);
+  // for (int i = 0; i < n; i++)
+  //     tree.insert(keys[i], i + 1, i + 1);
+
+  // cout << "Aoba" << endl;
+  // string wor2d = texto.substr(1, 4);
+  // cout << wor2d.compare("bear") << endl;
+
+  // cout << "Ao2ba" << endl;
+
+  // Para cada entrada processa se é uma palavra na árvore
+  Trie tree = Trie();
+  string word;
+  while (1) { // leitura das palavras ate' encontrar "0"
+    cin >> word;
+
+    if ((word.compare("dicionario1.dic") == 0) ||
+        (word.compare("dicionario2.dic") == 0)) {
+      fstream dictFile;
+      string texto;
+
+      // Lê o arquivo dic e coloca no objeto texto
+      dictFile.open(word, ios::in);
+      if (dictFile.is_open()) {
         string line;
         while (getline(dictFile, line)) {
-            texto += line;
+          texto += line;
         }
         dictFile.close();
-    }
+      }
 
-    cout << texto << endl;
-    // Processa a entrada e alimenta a árvore
-    // string keys[] = {"bear", "beara", "there", "answer", "any", "by", "bye", "their"};
-    // int n = sizeof(keys) / sizeof(keys[0]);
-    Trie tree = Trie();
-    // for (int i = 0; i < n; i++)
-    //     tree.insert(keys[i], i + 1, i + 1);
+      int textSize = texto.size();
+      int index = -1;
 
-    // cout << "Aoba" << endl;
-    // string wor2d = texto.substr(1, 4);
-    // cout << wor2d.compare("bear") << endl;
+      size_t wordIndexStart;
+      size_t wordIndexEnd;
+      size_t wordMeaningIndexEnd;
+      bool reading = false;
 
-    // cout << "Ao2ba" << endl;
-    int textSize = texto.size();
-    int index = -1;
-
-    size_t wordIndexStart;
-    size_t wordIndexEnd;
-    size_t wordMeaningIndexEnd;
-    bool reading = false;
-
-    for (; index < textSize;) {
+      for (; index < textSize;) {
         index++;
         if (texto[index] == '[') {
-            if (reading) { // Finaliza o processo da palavra anterior
-                // Atualiza o final da palavra atual
-                wordMeaningIndexEnd = index - 1;
-                size_t meaningSize = wordMeaningIndexEnd - wordIndexStart;
-                size_t wordSize = wordIndexEnd - wordIndexStart;
-                string word = texto.substr(wordIndexStart + 1, wordSize);
+          if (reading) { // Finaliza o processo da palavra anterior
+            // Atualiza o final da palavra atual
+            wordMeaningIndexEnd = index - 1;
+            size_t meaningSize = wordMeaningIndexEnd - wordIndexStart;
+            size_t wordSize = wordIndexEnd - wordIndexStart;
+            string word = texto.substr(wordIndexStart + 1, wordSize);
 
-                // cout << wordSize << endl;
-                // cout << meaningSize << endl;
-                // cout << wordIndexStart << endl;
-                // Insere na árvore
-                cout << "Inserindo a palavra " << word << endl;
-                tree.insert(word, wordIndexStart, meaningSize);
-                // Atualiza para a próxima palavra
-                wordIndexStart = index;
-            } else { // Não estava lendo, começou agora
-                wordIndexStart = index;
-                reading = true;
-            }
+            // cout << wordSize << endl;
+            // cout << meaningSize << endl;
+            // cout << wordIndexStart << endl;
+            // Insere na árvore
+            // cout << "Inserindo a palavra " << word << endl;
+            tree.insert(word, wordIndexStart, meaningSize);
+            // Atualiza para a próxima palavra
+            wordIndexStart = index;
+          } else { // Não estava lendo, começou agora
+            wordIndexStart = index;
+            reading = true;
+          }
         } else if (texto[index] == ']') { // Terminou de ler a palavra
-            wordIndexEnd = index - 1;
+          wordIndexEnd = index - 1;
         }
+      }
+      continue;
     }
 
-    // Para cada entrada processa se é uma palavra na árvore
-    string word;
-    while (1) { // leitura das palavras ate' encontrar "0"
-        cin >> word;
+    if (word.compare("0") == 0)
+      break;
 
-        if (word.compare("0") == 0)
-            break;
+    tree.processPrefix(word);
+  }
 
-        tree.processPrefix(word);
-    }
-
-    return 0;
+  return 0;
 }
